e72085ea728dc5388878f171e32c5159ca20bcd0
Fred Lee <juhee.fred.lee@gmail.com>
2022-07-01 17:08:19 +0900

    봉우리

--- src/Se2/Se2_10.java
+++ src/Se2/Se2_10.java
@@ -1,7 +1,38 @@
 package Se2;
 
-public class Se2_10 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Se2_10 {
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    public int solution(int n, int[][] arr) {
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 && nx <n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) answer++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Se2_10 T = new Se2_10();
+        Scanner stdIn = new Scanner(System.in);
+        int n = stdIn.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = stdIn.nextInt();
+            }
+        }
+        System.out.println(T.solution(n, arr));
     }
 }
